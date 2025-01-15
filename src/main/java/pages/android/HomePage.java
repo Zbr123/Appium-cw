ea49c21873c6a7ae4810c7fc5f0f061c62504d44
Ahsan Khan <ahsankhan@nekteck.com>
2025-01-15 16:24:06 +0500

    Cleaned Framework

--- src/main/java/pages/android/HomePage.java
+++ src/main/java/pages/android/HomePage.java
@@ -8,16 +8,6 @@ import pages.Page;
 
 public class HomePage {
 
-//    private String titleXpath = "//android.view.ViewGroup/android.widget.TextView";
-//    private String immediateButtonID = "com.meritnation.store.testingapp:id/btn1";
-//    private String flexibleButtonID = "com.meritnation.store.testingapp:id/btn2";
-//    private String versionCodeID = "com.meritnation.store.testingapp:id/ver_code";
-//    private String versionNameID = "com.meritnation.store.testingapp:id/ver_name";
-//    private String resultText = "com.meritnation.store.testingapp:id/txt";
-//    private String getTitle = "//android.widget.TextView[@text='The App']";
-//    private String TextFieldDemoApp = "messageInput";
-//    private String getSaveButton = "//android.view.ViewGroup[@content-desc='messageSaveBtn']/android.widget.TextView";
-
     private String titleAccessibilityID = "SignInScreen:AppLogo:logo";
     private String emailAccessibilityID = "SignInScreen:EmailInput:email";
     private String passwordAccessibilityID = "SignInScreen:PasswordInput:password";
@@ -31,17 +21,6 @@ public class HomePage {
     private String Dashboard_accessbilityID = "NewHomeScreen:SeniorName:seniorname";
     private String MenubtnXpath = "//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup";
     private String signoutbtnAccessibilityID = "SideMenu:LogOutEvent:logout";
-
-
-    private String immediateButtonID = "com.meritnation.store.testingapp:id/btn1";
-    private String flexibleButtonID = "com.meritnation.store.testingapp:id/btn2";
-    private String versionCodeID = "com.meritnation.store.testingapp:id/ver_code";
-    private String versionNameID = "com.meritnation.store.testingapp:id/ver_name";
-    private String resultText = "com.meritnation.store.testingapp:id/txt";
-    private String getTitle = "//android.widget.TextView[@text='The App']";
-    private String TextFieldDemoApp = "messageInput";
-    private String getSaveButton = "//android.view.ViewGroup[@content-desc='messageSaveBtn']/android.widget.TextView";
-
 
     public HomePage(Page page) {
     }
@@ -96,62 +75,5 @@ public class HomePage {
 
     public WebElement Signout_btn() {
         return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(signoutbtnAccessibilityID));
-    }
-
-    public WebElement getVersionCode() {
-        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id(versionCodeID)));
-    }
-
-    public WebElement getVersionName() {
-        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id(versionNameID)));
-    }
-
-    public WebElement getButton(int num) {
-        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id("com.meritnation.store.testingapp:id/btn" + num)));
-    }
-
-    public WebElement getImmediateButton() {
-        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id(immediateButtonID)));
-    }
-
-    public WebElement getFlexibleButton() {
-        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id(flexibleButtonID)));
-    }
-
-    public WebElement getResult() {
-        return (AndroidDriverSetup.getAndroidDriver().findElement(By.id(resultText)));
-    }
-
-    public void backButton() {
-        AndroidDriverSetup.getAndroidDriver().navigate().back();
-    }
-
-    public WebElement getTitleDemoApp() {
-        return (AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(getTitle)));
-    }
-
-    public WebElement getTabNameDemoApp(String name) {
-        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(name));
-    }
-
-    public WebElement getTextFieldDemoApp() {
-        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(TextFieldDemoApp));
-    }
-
-    public WebElement getSaveButtonDemoApp() {
-        return (AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(getSaveButton)));
-    }
-
-    public WebElement getEchoedTextDemoApp(String echo) {
-        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(echo));
-    }
-
-    public WebElement getOptionDemoApp(String option) {
-        return (AndroidDriverSetup.getAndroidDriver().findElementByAccessibilityId(option));
-    }
-
-    public void scrollToElementWithAccessibilityId(String accessibilityId) {
-        String uiAutomatorString = String.format("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().description(\"%s\"))", accessibilityId);
-        AndroidDriverSetup.getAndroidDriver().findElement(MobileBy.AndroidUIAutomator(uiAutomatorString));
     }
 }
